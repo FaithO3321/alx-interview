0030cf1e21b229ddfa412e0e3adbe1d64cc7a7fd
FaithO321 <faithomondi1331@gmail.com>
2024-06-27 14:45:56 -0700

    pascals triangle task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that generates Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to n rows.
+
+    Args:
+        n (int): Number of rows to generate.
+
+    Returns:
+        list: A list of lists representing Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j-1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
