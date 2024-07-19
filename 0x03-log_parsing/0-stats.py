07fbab51ccbe51d551cd18f9075dac2718eae99a
FaithO3321 <faithomondi1331@gmail.com>
2024-07-18 18:40:01 -0700

    all tasks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-
-def print_stats(total_size, status_codes):
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
-
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        line_count += 1
-
-        try:
-            parts = line.split()
-            size = int(parts[-1])
-            status = int(parts[-2])
-
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
             total_size += size
-            if status in status_codes:
-                status_codes[status] += 1
-        except (ValueError, IndexError):
-            pass
+            counter += 1
 
-        if line_count % 10 == 0:
-            print_stats(total_size, status_codes)
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-except KeyboardInterrupt:
-    print_stats(total_size, status_codes)
-    raise
+except Exception as err:
+    pass
 
-print_stats(total_size, status_codes)
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
