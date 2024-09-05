3b937caf5d7b9589c19d209cadecea034baa80fc
FaithO3321 <faithomondi1331@gmail.com>
2024-09-05 13:56:38 -0700

    prime numbers for games

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
+""" Prime Game """
 
-def isWinner(x, nums):
-    if x < 1 or not nums:
+
+def generatePrimeNumbers(limit):
+    """Generate list of primes up to limit"""
+    primeNumbers = []
+    sieveList = [True] * (limit + 1)
+    for potentialPrime in range(2, limit + 1):
+        if sieveList[potentialPrime]:
+            primeNumbers.append(potentialPrime)
+            for multiple in range(potentialPrime, limit + 1, potentialPrime):
+                sieveList[multiple] = False
+    return primeNumbers
+
+
+def isWinner(numRounds, roundValues):
+    """
+    Determine the winner of the game
+    """
+    if not numRounds or not roundValues:
         return None
-
-    def sieve_of_eratosthenes(n):
-        """Generate a list of prime numbers up to n
-        using the Sieve of Eratosthenes."""
-        sieve = [True] * (n + 1)
-        sieve[0] = sieve[1] = False
-        for i in range(2, int(n ** 0.5) + 1):
-            if sieve[i]:
-                for multiple in range(i * i, n + 1, i):
-                    sieve[multiple] = False
-        return sieve
-
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
-
-    # Cache the number of moves for each possible n
-    prime_counts = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
-
-    maria_wins = 0
-    ben_wins = 0
-
-    # Simulate each round
-    for n in nums:
-        if prime_counts[n] % 2 == 0:
-            ben_wins += 1
+    mariaScore = benScore = 0
+    for i in range(numRounds):
+        primes = generatePrimeNumbers(roundValues[i])
+        if len(primes) % 2 == 0:
+            benScore += 1
         else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
+            mariaScore += 1
+    if mariaScore > benScore:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif benScore > mariaScore:
         return "Ben"
-    else:
-        return None
+    return None
