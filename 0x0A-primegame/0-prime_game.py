ecd526773424613e3d3f456a826fb8e3b95f6d56
FaithO3321 <faithomondi1331@gmail.com>
2024-10-17 06:53:36 -0700

    prime game task

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-""" Prime Game Problem
-    Topic: Eratosthenes algorithm
-"""
+""" Prime Game """
 
 
-def isWinner(x, nums):
+def generatePrimeNumbers(limit):
+    """Generate list of primes up to limit"""
+    primeNumbers = []
+    sieveList = [True] * (limit + 1)
+    for potentialPrime in range(2, limit + 1):
+        if sieveList[potentialPrime]:
+            primeNumbers.append(potentialPrime)
+            for multiple in range(potentialPrime, limit + 1, potentialPrime):
+                sieveList[multiple] = False
+    return primeNumbers
+
+
+def isWinner(numRounds, roundValues):
     """
-    Determines the winner in the prime game using
-    Eratosthenes prime sieving algorithm
+    Determine the winner of the game
     """
-    Ben = 0
-    Maria = 0
-
-    for round in range(x):
-        playing_numbers = [num for num in range(2, nums[round] + 1)]
-        index = 0
-        # Sieve prime numbers per round
-        while (index < len(playing_numbers)):
-            current_prime = playing_numbers[index]
-            sieve_index = index + current_prime
-            while(sieve_index < len(playing_numbers)):
-                playing_numbers.pop(sieve_index)
-                sieve_index += current_prime - 1
-            index += 1
-        # Determine winner - if number of primes is even player 1 wins
-        # else player 2 wins. Player 2  also wins if there is only one
-        # number to pick from
-        prime_count = (len(playing_numbers))
-        if prime_count and prime_count % 2:
-            Maria += 1
-        else:
-            Ben += 1
-
-    if Ben == Maria:
+    if not numRounds or not roundValues:
         return None
-    return 'Ben' if Ben > Maria else 'Maria'
+    mariaScore = benScore = 0
+    for i in range(numRounds):
+        primes = generatePrimeNumbers(roundValues[i])
+        if len(primes) % 2 == 0:
+            benScore += 1
+        else:
+            mariaScore += 1
+    if mariaScore > benScore:
+        return "Maria"
+    elif benScore > mariaScore:
+        return "Ben"
+    return None
